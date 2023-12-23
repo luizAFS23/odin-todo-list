16641e16120a7f20c2c0bccb914eba6ca59024ab
luizAFS23 <luizafs.1500@gmail.com>
2023-12-23 15:00:15 -0300

    changes

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
+
+
 class createTodo{
     createTodos(){
-        
+
+    }
+}
+
+class titleTodo{
+    constructor(title){
+        this.title = title;
+    }
+
+    printTitleTodo(){
+        console.log(title);
     }
 }
 
 const todos = {
-
+    title: new titleTodo('Comprar castanha');
 }
+
+todos.title.printTitleTodo();
