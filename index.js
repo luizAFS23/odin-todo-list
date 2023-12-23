6c9d79b47a275afc89c2293b055b25b50fa241f6
luizAFS23 <luizafs.1500@gmail.com>
2023-12-23 16:19:23 -0300

    changes

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const prompt = require('prompt-sync')();
 
 class Todo{
     constructor(title, description, duedate){
@@ -18,24 +17,32 @@ class Todo{
         console.log('Description: ', this.description);
         console.log('Due Date: ', this.duedate);
     }
+
+    toString(){
+        return `Title: ${this.title}, Description: ${this.description}, Due Date: ${this.duedate}`;
+    }
 }
 
 
 const todos = [];
 
-const titleform = document.getElementById('title').value;
-const descriptionform = document.getElementById('description').value;
-const duedateform = document.getElementById('duedate').value;
-const todo_ul_container = document.querySelector('.todo-ul-container').value;
+const titleform = document.getElementById('title');
+const descriptionform = document.getElementById('description');
+const duedateform = document.getElementById('duedate');
+const todo_ul_container = document.querySelector('.todo-ul-container');
+const form = document.getElementById('form');
 
 function createTodo(){
     const todo = new Todo(titleform, descriptionform, duedateform);
     todos.push(todo);
 }
 
-function createTodoList(){
+function createTodoList(e){
+    e.preventDefault();
+    createTodo();
     todos.forEach(todo => {
-        
+        todo_ul_container.outerHTML += `<li>${todo}</li>`;
     });
 }
 
+form.addEventListener('submit', (e) => createTodoList(e));
