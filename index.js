6303da2bf1789f122d8b7d180f4f91cfb48e03ee
luizAFS23 <luizafs.1500@gmail.com>
2023-12-23 15:43:19 -0300

    changes

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
 const prompt = require('prompt-sync')();
 
 class Todo{
-    constructor(title, description){
+    constructor(title, description, duedate){
         this.title = title;
         this.description = description;
+        this.duedate = duedate;
     }
 
     createaANewTodo(){
         todos.push(this.title);
         todos.push(this.description);
+        todos.push(this.duedate);
     }
 
     showTodo(){
         console.log('Title: ', this.title);
         console.log('Description: ', this.description);
+        console.log('Due Date: ', this.duedate);
     }
 }
 
 
 const todos = [];
 
+const titleform = document.getElementById('title').value;
+const descriptionform = document.getElementById('description').value;
+const duedateform = document.getElementById('duedate').value;
+const todo_ul_container = document.querySelector('.todo-ul-container').value;
 
+function createTodo(){
+    const todo = new Todo(titleform, descriptionform, duedateform);
+    todos.push(todo);
+}
 
-const new_todo_title = prompt('Please choose a title for your todo: ');
-const new_todo_description = prompt('Please choose a description for your todo: ');
-const todo = new Todo(new_todo_title, new_todo_description);
+function createTodoList(){
+    todos.forEach(todo => {
+        
+    });
+}
 
-
-todo.showTodo();
