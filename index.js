37cb21c9925eaae9690bf0d4805296fc55afbea4
luizAFS23 <luizafs.1500@gmail.com>
2023-12-23 15:16:38 -0300

    changes

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-const prompt = require('prompt-sync');
+const prompt = require('prompt-sync')();
 
 class createTodo{
+    constructor(title, description){
+        this.title = title;
+        this.description = description;
+    }
+
+    createaANewTodo(){
+        todos.push(this.title);
+        todos.push(this.description);
+    }
 
 }
 
@@ -9,17 +18,30 @@ class TitleTodo{
         this.title = title;
     }
 
+    returnTodoTitle(){
+        return this.title;
+    }
     
 }
 
-const todos = [
-    new TitleTodo('Comprar castanha'),
+class TodoDescription{
+    constructor(description){
+        this.description = description;
+    }
 
-]
+    returnTodoDescription(){
+        return this.description;
+    }
+}
+
+const todos = [];
 
 
 
 const new_todo_title = prompt('Please choose a title for your todo: ');
+const new_todo_description = prompt('Please choose a description for your todo: ');
+const todo = new createTodo(new_todo_title, new_todo_description);
 
+console.log(todo);
 
 
